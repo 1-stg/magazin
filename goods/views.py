e7f9a79938545bf290c900abb0208cc298d09529
1-stg <kabakov_glebochka@mail.ru>
2025-08-04 12:18:39 +0300

    14. Создание поиска товаров по сайту

--- goods/views.py
+++ goods/views.py
@@ -2,18 +2,25 @@ from importlib.resources import contents
 from django.http import HttpResponse
 from django.shortcuts import get_list_or_404, render
 from django.core.paginator import Paginator
+from .utils import q_search
 
 from goods.models import Products
 
-def catalog(request, category_slug) -> HttpResponse:
+def catalog(request, category_slug=None) -> HttpResponse:
 
     page = request.GET.get('page', 1 )
     on_sale = request.GET.get('on_sale', None)
     order_by = request.GET.get('order_by', None)
+    print(request.GET)
+    query = request.GET['q']
+
+    
 
 
     if category_slug == 'all':
         goods = Products.objects.all()
+    elif query:
+        goods = q_search(query)
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
     
