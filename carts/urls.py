fe47d7dd1efa9052f5c51a00e245aa5397149f00
1-stg <kabakov_glebochka@mail.ru>
2025-08-06 18:29:11 +0300

    свободный коммит

--- carts/urls.py
+++ carts/urls.py
@@ -5,13 +5,7 @@ from carts import views
 app_name = 'carts'
 
 urlpatterns = [
-<<<<<<< HEAD
     path('cart_add/', views.cart_add, name='cart_add'),
     path('cart_change/', views.cart_change, name='cart_change'),
     path('cart_remove/', views.cart_remove, name='cart_remove'),
-=======
-    path('cart_add/<slug:product_slug>/', views.cart_add, name='cart_add'),
-    path('cart_change/<slug:product_slug>/', views.cart_change, name='cart_change'),
-    path('cart_remove/<int:cart_id>/', views.cart_remove, name='cart_remove'),
->>>>>>> e1ded3d2c89a029f73d0bda48b593df9743132a2
 ]
